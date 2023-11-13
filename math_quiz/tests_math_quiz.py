d9521325155cf9a33e9e2f71062df3f669eba5b4
Udaygitin <Udayputta99@gmail.com>
2023-11-13 23:42:48 +0100

    Added unit tests for math_quiz functions

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -2,29 +2,47 @@ import unittest
 from math_quiz import function_A, function_B, function_C
 
 
-class TestMathGame(unittest.TestCase):
+import unittest
+from unittest.mock import patch
+from math_quiz import generate_random_integer, generate_random_operator, perform_operation, math_quiz
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+class TestMathQuizFunctions(unittest.TestCase):
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_random_integer(self):
+        minimum, maximum = 1, 10
+        random_integer = generate_random_integer(minimum, maximum)
+        self.assertGreaterEqual(random_integer, minimum)
+        self.assertLessEqual(random_integer, maximum)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_generate_random_operator(self):
+        operators = {'+', '-', '*'}
+        random_operator = generate_random_operator()
+        self.assertIn(random_operator, operators)
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+    def test_perform_operation_addition(self):
+        result = perform_operation(3, 4, '+')
+        self.assertEqual(result, ('3 + 4', 7))
 
-if __name__ == "__main__":
+    def test_perform_operation_subtraction(self):
+        result = perform_operation(8, 5, '-')
+        self.assertEqual(result, ('8 - 5', 3))
+
+    def test_perform_operation_multiplication(self):
+        result = perform_operation(2, 6, '*')
+        self.assertEqual(result, ('2 * 6', 12))
+
+    @patch('builtins.input', side_effect=['7'])
+    @patch('builtins.print')
+    def test_math_quiz_correct_answer(self, mock_print, mock_input):
+        math_quiz()
+        mock_print.assert_any_call("Correct! You earned a point.")
+
+    @patch('builtins.input', side_effect=['10'])
+    @patch('builtins.print')
+    def test_math_quiz_wrong_answer(self, mock_print, mock_input):
+        math_quiz()
+        mock_print.assert_any_call("Wrong answer. The correct answer is ")
+
+if __name__ == '__main__':
     unittest.main()
+
