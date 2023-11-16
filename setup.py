06b2af2f3471d049d51edb359733b819bee3f052
Udaygitin <Udayputta99@gmail.com>
2023-11-16 21:48:19 +0100

    Added files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='math_quiz',
+   version='1.0.0',
+   description='Math quiz module',
+   author='Udaya Bhaskar Putta',
+   author_email='udaya.putta@fau.de',
+   packages=['math_quiz'],
+   license='Apache 2.0',
+   install_requires=[]
+)
